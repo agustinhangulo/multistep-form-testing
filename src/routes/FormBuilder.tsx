9e5c8a816b69e74feab1e8d06bd1e96a13ca2583
Agustin A. <agustinangulo562@gmail.com>
2025-06-26 14:51:18 -0700

    feat: stepper component to show current step of multistep form

--- src/routes/FormBuilder.tsx
+++ src/routes/FormBuilder.tsx
@@ -1,11 +1,10 @@
+import { Stepper } from "@/features/multistepForm/components/Stepper";
 import { Outlet } from "react-router";
 
 export const FormBuilder = () => {
   return (
     <div className="flex flex-col gap-4 m-4">
-      <p className="p-2 border border-stone-800">
-        In the future this will be a stepper/progress component
-      </p>
+      <Stepper />
       <Outlet />
     </div>
   );
